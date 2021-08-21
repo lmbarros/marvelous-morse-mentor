5e465ee7677b63a8f02c42cbdfdca420eb67a2f9
Leandro Motta Barros <lmb@stackedboxes.org>
2021-08-20 21:59:42 -0300

    Buzzing buzzer and displaying display

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,12 @@ FROM balenalib/raspberry-pi-python:3-buster-build-20210705
 
 WORKDIR /src
 
-RUN git clone https://github.com/Seeed-Studio/grove.py && cd grove.py && sudo pip3 install .
-RUN pip3 install RPi.GPIO
+RUN apt-get update && apt-get install apt-transport-https ca-certificates
+RUN echo "deb https://seeed-studio.github.io/pi_repo/ stretch main" | tee /etc/apt/sources.list.d/seeed.list
+RUN apt-key adv --keyserver keyserver.ubuntu.com --recv-keys BB8F40F3
+RUN apt-get update && apt-get install python3-mraa python3-upm python3-rpi.gpio
+RUN pip3 install rpi_ws281x
+RUN git clone https://github.com/Seeed-Studio/grove.py && cd grove.py && pip3 install .
 
 COPY src /src
 
--- src/main.py
+++ src/main.py
@@ -1,20 +1,61 @@
 import time
 from grove.grove_led import GroveLed
+from grove.factory import Factory
+
+# Transform this into simple functions, maybe in a separate file, probably using
+# a "start buzz/end buzz" interface ('cause that's what I'll need).
+class Buzzer():
+    def __init__(self):
+        from mraa import getGpioLookup
+        from upm import pyupm_buzzer as upmBuzzer
+
+        mraa_pin = getGpioLookup("GPIO12") # PWM pin always 12 on Grove?
+        self.buzzer = upmBuzzer.Buzzer(mraa_pin)
+
+        self.buzzer.setVolume(1/50)
+
+        print("Initialized buzzer '{}'".format(self.buzzer.name()))
+
+
+    def play(self, note, time):
+        self.buzzer.playSound(note, time)
+
 
 def main():
-    bluePin = 5
-    greenPin = 16
+    ledPin = 5
+    buttonPin = 16
+    joyPin = 0
 
-    blueLED = GroveLed(bluePin)
-    greenLED = GroveLed(greenPin)
+    # LED
+    theLED = GroveLed(ledPin)
 
+    # Display
+    lcd = Factory.getDisplay("JHD1802")
+    rows, cols = lcd.size()
+    print("LCD model: {}".format(lcd.name))
+    print("LCD type : {} x {}".format(cols, rows))
+    lcd.clear()
+
+    # Buzzer
+    buzzer = Buzzer()
+
+    counter = 1
     while True:
-        blueLED.on()
-        greenLED.off()
+        theLED.on()
         time.sleep(1.5)
-        blueLED.off()
-        greenLED.on()
-        time.sleep(0.15)
+
+        lcd.setCursor(0, 0)
+        lcd.write("Arre!")
+        lcd.setCursor(0, cols - 1)
+        lcd.write('X')
+        lcd.setCursor(rows - 1, 0)
+        lcd.write(str(counter))
+        counter += 1
+        buzzer.play(2500, 500000)
+
+        theLED.off()
+        time.sleep(1.5)
+
 
 
 if __name__ == '__main__':
