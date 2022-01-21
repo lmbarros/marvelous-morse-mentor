bba345ecfd25ea540b4fa315380475186e4bfcf3
Leandro Motta Barros <lmb@stackedboxes.org>
2022-01-21 17:16:55 -0300

    Update copyright, fix typo
    A typo in the project name. Oh well.

--- mmm/src/welcome_state.py
+++ mmm/src/welcome_state.py
@@ -1,6 +1,6 @@
 #
-# Marvelous Morse Menthor
-# Copyright 2021 Leandro Motta Barros
+# Marvelous Morse Mentor
+# Copyright 2021-2022 Leandro Motta Barros
 # Licensed under the Apache License 2.0 (see the LICENSE file for details)
 #
 
